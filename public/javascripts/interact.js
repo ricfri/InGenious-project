c8482ce2458e47f97826034f8a98b9cab9b71ac8
Richard Almgren Friberg <rical803@student.liu.se>
2018-12-17 15:38:15 +0100

    Dragging the glasses should now be doable on mobile.

--- public/javascripts/interact.js
+++ public/javascripts/interact.js
@@ -1,6 +1,7 @@
 /* The dragging code for '.draggable' from the demo above
  * applies to this demo as well so it doesn't have to be repeated. */
 var interactIndex = 1
+interact.pointerMoveTolerance(5);
 
 // enable draggables to be dropped into this
 interact('.dropzone').dropzone({
@@ -22,13 +23,11 @@ interact('.dropzone').dropzone({
     // feedback the possibility of a drop
     dropzoneElement.classList.add('drop-target');
     draggableElement.classList.add('can-drop');
-    draggableElement.textContent = 'Dragged in';
   },
   ondragleave: function (event) {
     // remove the drop feedback style
     event.target.classList.remove('drop-target');
     event.relatedTarget.classList.remove('can-drop');
-    event.relatedTarget.textContent = 'Dragged out';
   },
   ondrop: function (event) {
     document.getElementById(interactIndex.toString()).style.display = 'none';
