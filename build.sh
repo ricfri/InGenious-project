664dc694efba91f867e2907147677ebad887dd9f
Richard Almgren Friberg <rical803@student.liu.se>
2018-12-17 20:51:15 +0100

    changed so build produces everything in /public

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-rm -rf dist/
-mkdir dist
-mkdir dist/stylesheets
-mkdir dist/javascripts
-mkdir dist/images
-echo "dist folder reset"
+rm -rf public/
+mkdir public
+mkdir public/stylesheets
+mkdir public/javascripts
+mkdir public/images
+echo "public folder reset"
 HTML_FILE_NAME="index.html"
 HTML_FILE_NAME_404="404.html"
-HTML_FILE_LOCATION="public/"
+HTML_FILE_LOCATION="dev/"
 CSS_FILE_NAME="design.css"
-CSS_FILE_LOCATION="public/stylesheets/"
+CSS_FILE_LOCATION="dev/stylesheets/"
 JS_FILE_NAME="interact.js"
-JS_FILE_LOCATION="public/javascripts/"
-html-minifier --collapse-whitespace $HTML_FILE_LOCATION$HTML_FILE_NAME -o dist/$HTML_FILE_NAME
-html-minifier --collapse-whitespace $HTML_FILE_LOCATION$HTML_FILE_NAME_404 -o dist/$HTML_FILE_NAME_404
+JS_FILE_LOCATION="dev/javascripts/"
+html-minifier --collapse-whitespace $HTML_FILE_LOCATION$HTML_FILE_NAME -o public/$HTML_FILE_NAME
+html-minifier --collapse-whitespace $HTML_FILE_LOCATION$HTML_FILE_NAME_404 -o public/$HTML_FILE_NAME_404
 echo "$HTML_FILE_NAME minified"
-cp $CSS_FILE_LOCATION$CSS_FILE_NAME dist/stylesheets/
-uglifyjs --compress --mangle -o dist/javascripts/$JS_FILE_NAME -- $JS_FILE_LOCATION$JS_FILE_NAME
+cp $CSS_FILE_LOCATION$CSS_FILE_NAME public/stylesheets/
+uglifyjs --compress --mangle -o public/javascripts/$JS_FILE_NAME -- $JS_FILE_LOCATION$JS_FILE_NAME
 echo "$JS_FILE_NAME minified"
-node imgcompress.js --in="public/images" --out="dist/images"
+node imgcompress.js --in="dev/images" --out="public/images"
 echo "images minified"
 echo "Minification complete!"
